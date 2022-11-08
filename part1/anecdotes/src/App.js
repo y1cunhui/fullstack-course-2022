8368d9bceeac56761bf9f81d4f9dcb8794c7327f
Xavier-YangCN <38470282+Xavier-YangCN@users.noreply.github.com>
2022-11-08 12:06:19 +0800

    exercise 1.12-1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+
+  
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Uint32Array(anecdotes.length))
+
+  const randomAnecdote = () => {
+    const r = Math.round(Math.random() * (anecdotes.length - 1))
+    setSelected(r)
+  }
+
+  const voteForOne = () => {
+    let copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const maxVoted = 
+    anecdotes.map((x, i) => [x, i]).reduce((r, a) => (
+      votes[r[1]] > votes[a[1]] ? r : a
+    ))[0]
+  
+  
+  const maxVotes = 
+      votes.reduce((r, a) => (
+        r > a ? r : a
+      ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={voteForOne}>vote</button>
+      <button onClick={randomAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{maxVoted}</p>
+      <p>has {maxVotes} votes</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
